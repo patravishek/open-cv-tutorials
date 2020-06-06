80461585720d2dddc1afa5d602f1606fda7ad20c
Avishek Patra <avishek.patra@outlook.com>
2020-06-07 03:57:54 +0530

    Some refactoring the code

--- img_rgb.py
+++ img_rgb.py
@@ -6,15 +6,12 @@ import matplotlib.pyplot as plt
 
 # Import the image
 img = cv2.imread('popeye.jpg')
-plt.imshow(img)
 
 # Convert the image into RGB
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(img_rgb)
 
 # Convert the image into gray scale
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-plt.imshow(img_gray, cmap = 'gray')
 
 # Write the images
 cv2.imwrite('popeye_rgb.jpg', img_rgb)
