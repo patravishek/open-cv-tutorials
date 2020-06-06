3d1369dda7e238827c9ae80756a4481282288559
Avishek Patra <avishek.patra@outlook.com>
2020-06-07 04:05:48 +0530

    Blurring image

--- img_rgb.py
+++ img_rgb.py
@@ -13,6 +13,14 @@ img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 # Convert the image into gray scale
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Convert the image to HSV
+img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# Blurring the image
+figure_size = 12 # Dimension of the x and y axis of the karnel
+img_blur = cv2.blur(img,(figure_size, figure_size))
+
 # Write the images
 cv2.imwrite('popeye_rgb.jpg', img_rgb)
 cv2.imwrite('popeye_gray.jpg', img_gray)
+cv2.imwrite('popeye_blur.jpg', img_blur)
